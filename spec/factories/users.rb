9b4b4a1d31c98ab9ca2d8f6ce796d66075759cc9
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-05-05 14:01:33 +0700

    Initial User specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    email                       { Faker::Internet.email }
+    username                    { Faker::Internet.user_name }
+    password 'password01'
+    password_confirmation 'password01'
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
+
+  describe '.username' do
+    it { expect(FactoryGirl.build(:user, username: '')).to_not be_valid }
+
+    it 'is unique' do
+      FactoryGirl.create(:user, username: 'a')
+
+      expect(FactoryGirl.build(:user, username: 'a')).to_not be_valid
+    end
+  end
 end
