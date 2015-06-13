fff675bef3472861a88b0d670602d494a68757fa
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-06-13 16:14:48 +0700

    Route root to sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: redirect('users/sign_in')
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
