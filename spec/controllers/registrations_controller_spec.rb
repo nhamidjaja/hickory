114e8ad5b7967dcda826f76b8243a1132ee6ab30
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-06-27 17:13:01 +0700

    Fix controller spec with mock hash

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -5,9 +5,7 @@ RSpec.describe RegistrationsController, type: :controller do
 
   describe '#new' do
     context 'has omniauth session' do
-      let(:info) { double(email: 'a@b.com') }
-      let(:credentials) { double(token: 'abc098') }
-      let(:callback) { double(provider: 'facebook', uid: '123', info: info, credentials: credentials) }
+      let(:callback) { { 'provider' => 'facebook', 'uid' => '123', 'info' => { 'email' => 'a@b.com' }, 'credentials' => { 'token' => 'abc098' } } }
 
       before do
         request.session[:omniauth] = callback
