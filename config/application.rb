77e185f2bc7969bc429101121b14b54b947fef92
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-05 18:53:21 +0700

    Move Fave::Uri into app folder

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,16 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'fave/uri'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Hickory
   class Application < Rails::Application
+    # Custom code goes here
+    config.autoload_paths << "#{config.root}/app/src"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
