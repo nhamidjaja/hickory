2de710d57ff4e0a6ad9e5b693db5135cc5f70f00
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-06 15:42:55 +0700

    Initial personal fave POC

--- app/controllers/fave_controller.rb
+++ app/controllers/fave_controller.rb
@@ -2,5 +2,18 @@ class FaveController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    url = Fave::Url.new(params[:url])
+
+
+    # Move everything below to worker
+    article = MasterFeed.new
+
+    begin
+      article = MasterFeed.find(url.canon)
+    rescue Cequel::Record::RecordNotFound
+    end
+
+    PersonalFave.create(user_id: current_user.id, content_url: url.canon,
+      headline: article.headline, header_image_url: article.header_image_url, published_at: article.published_at)
   end
 end
