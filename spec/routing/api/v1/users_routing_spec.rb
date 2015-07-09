348f13608da152c84bd0f59eb3486a54a6184411
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-09 17:38:58 +0700

    Rubocop correction

--- spec/routing/api/v1/users_routing_spec.rb
+++ spec/routing/api/v1/users_routing_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'users routes', type: :routing do
-  it { expect(get('/api/v1/users/some-id')).to route_to(controller: 'api/v1/users', action: 'show', id: 'some-id') }
+  it do
+    expect(get('/api/v1/users/some-id'))
+      .to route_to(controller: 'api/v1/users', action: 'show', id: 'some-id')
+  end
 end
