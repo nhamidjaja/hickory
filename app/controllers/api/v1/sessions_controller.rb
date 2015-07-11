b55357d8130226f4e3ae3d5602a9c026c3de605a
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-11 15:54:59 +0700

    Fetch fb user with Graph API

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,9 +2,15 @@ module Api
   module V1
     class SessionsController < Api::V1::ApplicationController
       skip_before_action :authenticate_user_from_token!
+      rescue_from FbGraph2::Exception::InvalidToken, with: :render_unauthorized
 
       def facebook
-        fail Errors::NotAuthorized
+        token = request.headers['X-Facebook-Token']
+        fail Errors::NotAuthorized unless token
+
+        fb_user = nil
+        me = FbGraph2::User.me(token)
+        fb_user = me.fetch
       end
     end
   end
--- spec/requests/api/v1/sessions_requests_spec.rb
+++ spec/requests/api/v1/sessions_requests_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe 'sessions', type: :request do
 
     it { expect(response.status).to eq(401) }
   end
+
+  context 'with invalid token' do
+    before do
+      expect_any_instance_of(FbGraph2::User)
+        .to receive(:fetch).and_raise(FbGraph2::Exception::InvalidToken, 'Invalid Token')
+      get '/api/v1/sessions/facebook',
+        nil,
+        'X-Facebook-Token' => 'invalid-token'
+    end
+
+    it { expect(response.status).to eq(401) }
+  end
 end
