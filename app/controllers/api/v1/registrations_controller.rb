d5a779648fe3cd0f3ed92793f5455c6e3b94b11f
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-12 00:01:13 +0700

    User registration API through facebook token

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,6 +1,26 @@
 module Api
   module V1
     class RegistrationsController < Api::V1::ApplicationController
+      skip_before_action :authenticate_user_from_token!
+      rescue_from FbGraph2::Exception::InvalidToken, with: :render_unauthorized
+
+      def facebook
+        token = request.headers['X-Facebook-Token']
+        fail(Errors::NotAuthorized, 'No Facebook token provided') unless token
+
+        fb_user = FbGraph2::User.me(token).fetch
+        user = User.new
+        user.apply_third_party_auth(Fave::Auth.from_facebook(fb_user))
+        user.username = params[:username]
+
+        if user.save
+          sign_in user, store: false
+          render 'facebook', status: 201
+        else
+          warden.custom_failure!
+          render json: { errors: user.errors }, status: 400
+        end
+      end
     end
   end
 end
--- spec/requests/api/v1/registrations_requests_spec.rb
+++ spec/requests/api/v1/registrations_requests_spec.rb
@@ -1,4 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe 'User Registrations API', type: :request do
+  context 'no token' do
+    before do
+      get '/api/v1/registrations/facebook'
+    end
+
+    it { expect(response.status).to eq(401) }
+    it { expect(json['errors']['message']).to match('No Facebook token provided')}
+  end
+
+  context 'with invalid token' do
+    before do
+      expect_any_instance_of(FbGraph2::User)
+        .to receive(:fetch)
+        .and_raise(FbGraph2::Exception::InvalidToken, 'Invalid token')
+      get '/api/v1/registrations/facebook',
+          nil,
+          'X-Facebook-Token' => 'invalid-token'
+    end
+
+    it { expect(response.status).to eq(401) }
+    it { expect(json['errors']['message']).to match('Invalid token')}
+  end
+
+  context 'valid token' do
+    before do
+      double = instance_double(
+        'FbGraph2::User',
+        email: 'new@email.com',
+        id: 'x123',
+        access_token: 'fb-token'
+      )
+
+      expect_any_instance_of(FbGraph2::User)
+        .to receive(:fetch)
+        .and_return(double)
+    end
+
+    context 'create new user' do
+      before do
+        get '/api/v1/registrations/facebook?username=nicholas',
+            nil,
+            'X-Facebook-Token' => 'fb-token'
+      end
+
+      it { expect(response.status).to eq(201) }
+      it { expect(json['user']['email']).to match('new@email.com') }
+      it { expect(json['user']['username']).to match('nicholas') }
+      it { expect(json['user']['authentication_token']).to_not be_blank }
+    end
+
+    context 'invalid user metadata' do
+      before do
+        get '/api/v1/registrations/facebook',
+            nil,
+            'X-Facebook-Token' => 'fb-token'
+      end
+
+      it { expect(response.status).to eq(400) }
+      it { expect(json['errors']['username']).to eq(['is invalid']) }
+    end
+  end
 end
