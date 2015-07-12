19ca65f613ea8138dd5141c53ee5249e19d2e3f2
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-12 10:53:05 +0700

    Use facebook app config from figaro

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.omniauth :facebook, Rails.application.config.facebook_app_id, Rails.application.config.facebook_app_secret, { scope: 'email,user_friends' }
+  config.omniauth :facebook, Figaro.env.facebook_app_id, Figaro.env.facebook_app_secret, { scope: 'email,user_friends' }
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
