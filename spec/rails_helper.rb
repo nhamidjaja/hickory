18ea5f2b999cf38056526636c7f864f9733cb854
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-19 15:28:04 +0700

    Use sidekiq testing helper to not execute queues

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'sidekiq/testing'
+Sidekiq::Testing.fake!
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'simplecov'
 SimpleCov.start 'rails'
