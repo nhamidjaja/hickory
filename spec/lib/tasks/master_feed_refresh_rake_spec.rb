157a243a6f514e80f32b0ff5985e988032b50adb
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-20 12:32:57 +0700

    Rubocop corrections

--- spec/lib/tasks/master_feed_refresh_rake_spec.rb
+++ spec/lib/tasks/master_feed_refresh_rake_spec.rb
@@ -3,20 +3,20 @@ require 'rake'
 
 RSpec.describe 'master_feed namespace task' do
   before :all do
-    Rake.application.rake_require "tasks/master_feed"
+    Rake.application.rake_require 'tasks/master_feed'
     Rake::Task.define_task(:environment)
   end
 
   describe 'master_feed:refresh' do
     let :run_rake_task do
-      Rake::Task["master_feed:refresh"].reenable
-      Rake.application.invoke_task "master_feed:refresh"
+      Rake::Task['master_feed:refresh'].reenable
+      Rake.application.invoke_task 'master_feed:refresh'
     end
 
     it 'clears queue' # do
-      # expect_any_instance_of('Sidekiq::Queue').to receive(:clear).once
+    # expect_any_instance_of('Sidekiq::Queue').to receive(:clear).once
 
-      # run_rake_task
+    # run_rake_task
     # end
   end
 end
