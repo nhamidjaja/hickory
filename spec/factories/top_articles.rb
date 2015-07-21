878cdb1a8d5b039b868495affde5ecbfa414b599
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-21 13:01:56 +0700

    Replace MasterFeed cequel model with TopArticle activerecord model

---
+++ spec/factories/top_articles.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :top_article do
+    content_url { Fave::Url.new(Faker::Internet.url).canon }
+    feeder { FactoryGirl.create(:feeder) }
+    title "MyString"
+    image_url "MyString"
+    published_at "2015-07-20 19:01:10"
+  end
+
+end
