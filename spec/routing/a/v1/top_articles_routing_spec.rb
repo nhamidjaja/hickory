c01611d10eed9d76c80ee400f7c25d596a7d8fbb
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-29 14:10:58 +0700

    Test route for top_articles#index

---
+++ spec/routing/a/v1/top_articles_routing_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'top articles routes', type: :routing do
+  it do
+    expect(get('/a/v1/top_articles'))
+      .to route_to(controller: 'a/v1/top_articles', action: 'index', format: :json)
+  end
+end
