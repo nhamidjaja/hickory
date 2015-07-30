80dda456f68393dd4626b283c85844b32aea410a
Phendy Chandra <phendy@weekendinc.com>
2015-07-30 13:30:45 +0700

    View user detail api

--- app/controllers/a/v1/users_controller.rb
+++ app/controllers/a/v1/users_controller.rb
@@ -1,6 +1,11 @@
 module A
   module V1
     class UsersController < A::V1::ApplicationController
+      respond_to :json
+
+      def show
+        @user = User.find(params[:id])
+      end
     end
   end
 end
--- spec/requests/a/v1/users_requests_spec.rb
+++ spec/requests/a/v1/users_requests_spec.rb
@@ -1,4 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
+  context 'unauthenticated' do
+    it 'is unauthorized' do
+      get '/a/v1/top_articles'
+
+      expect(response.status).to eq(401)
+      expect(json['errors']).to_not be_blank
+    end
+  end
+
+  context 'authorized' do
+    before do
+      FactoryGirl.create(:user,
+                         id: '4f16d362-a336-4b12-a133-4b8e39be7f8e',
+                         email: 'a@user.com',
+                         username: 'user',
+                         authentication_token: 'validtoken')
+    end
+
+    context 'view user' do
+      context 'with not exists user id' do
+        it 'is 500 user not found' do
+          get '/a/v1/users/99a89669-557c-4c7a-a533-d1163caad65f',
+              nil,
+              'X-Email' => 'a@user.com',
+              'X-Auth-Token' => 'validtoken'
+
+          expect(response.status).to eq(500)
+          expect(json['errors']).to_not be_blank
+        end
+      end
+
+      context 'with exists user id' do
+        it 'show user detail' do
+          get '/a/v1/users/4f16d362-a336-4b12-a133-4b8e39be7f8e',
+              nil,
+              'X-Email' => 'a@user.com',
+              'X-Auth-Token' => 'validtoken'
+
+          expect(json['user']['username']).to eq('user')
+        end
+      end
+    end
+  end
 end
