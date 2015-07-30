5d349f30390b397d18abb87df040dbfc7cf5f16a
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-30 12:52:43 +0700

    Remove unused search API specs

--- spec/requests/a/v1/search_requests_spec.rb
+++ spec/requests/a/v1/search_requests_spec.rb
@@ -85,39 +85,5 @@ RSpec.describe 'Search API', type: :request do
         end
       end
     end
-
-    # context 'search user' do
-    #   it 'not found and search by prefix' do
-    #     get '/a/v1/search?query=ser',
-    #         nil,
-    #         'X-Email' => 'a@user.com',
-    #         'X-Auth-Token' => 'validtoken'
-
-    #     expect(json['users']).to eq([])
-    #   end
-
-    #   it 'found 1 user' do
-    #     get '/a/v1/search?query=user',
-    #         nil,
-    #         'X-Email' => 'a@user.com',
-    #         'X-Auth-Token' => 'validtoken'
-
-    #     expect(json['users'][0]['username']).to eq('user1')
-    #   end
-
-    #   it 'found 2 data' do
-    #     FactoryGirl.create(:user,
-    #                        email: 'ab@userb.com',
-    #                        username: 'user2',
-    #                        authentication_token: 'validtokenb')
-
-    #     get '/a/v1/search?query=user',
-    #         nil,
-    #         'X-Email' => 'a@user.com',
-    #         'X-Auth-Token' => 'validtoken'
-
-    #     expect(json['users'].size).to eq(2)
-    #   end
-    # end
   end
 end
