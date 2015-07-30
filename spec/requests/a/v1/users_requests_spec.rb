2605b2d6909a3c1f79b08e4f23e169e02a658f43
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-07-30 15:17:14 +0700

    Refactor get user by id API

--- spec/requests/a/v1/users_requests_spec.rb
+++ spec/requests/a/v1/users_requests_spec.rb
@@ -13,34 +13,36 @@ RSpec.describe 'Users API', type: :request do
   context 'authorized' do
     before do
       FactoryGirl.create(:user,
-                         id: '4f16d362-a336-4b12-a133-4b8e39be7f8e',
                          email: 'a@user.com',
-                         username: 'user',
                          authentication_token: 'validtoken')
     end
 
-    context 'view user' do
-      context 'with not exists user id' do
-        it 'is 404 user not found' do
-          get '/a/v1/users/99a89669-557c-4c7a-a533-d1163caad65f',
-              nil,
-              'X-Email' => 'a@user.com',
-              'X-Auth-Token' => 'validtoken'
+    context 'user does not exist' do
+      it 'is not found' do
+        get '/a/v1/users/id-not-in-system',
+            nil,
+            'X-Email' => 'a@user.com',
+            'X-Auth-Token' => 'validtoken'
 
-          expect(response.status).to eq(404)
-          expect(json['errors']).to_not be_blank
-        end
+        expect(response.status).to eq(404)
+        expect(json['errors']).to_not be_blank
       end
+    end
 
-      context 'with exists user id' do
-        it 'show user detail' do
-          get '/a/v1/users/4f16d362-a336-4b12-a133-4b8e39be7f8e',
-              nil,
-              'X-Email' => 'a@user.com',
-              'X-Auth-Token' => 'validtoken'
+    context 'user exists' do
+      it 'is successful' do
+        FactoryGirl.create(:user,
+                           id: '4f16d362-a336-4b12-a133-4b8e39be7f8e',
+                           username: 'xyz')
 
-          expect(json['user']['username']).to eq('user')
-        end
+        get '/a/v1/users/4f16d362-a336-4b12-a133-4b8e39be7f8e',
+            nil,
+            'X-Email' => 'a@user.com',
+            'X-Auth-Token' => 'validtoken'
+
+        expect(response.status).to eq(200)
+        expect(json['user']['id']).to eq('4f16d362-a336-4b12-a133-4b8e39be7f8e')
+        expect(json['user']['username']).to eq('xyz')
       end
     end
   end
