cbef2b58612519fb551016ca1ac7a79bcf8abcfc
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-08-03 18:45:28 +0700

    Rearrange FController#index spec

--- spec/controllers/f_controller_spec.rb
+++ spec/controllers/f_controller_spec.rb
@@ -1,27 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe FController, type: :controller do
-  context 'unsigned in user' do
-    describe 'GET #index' do
+  describe 'GET #index' do
+    context 'unsigned in user' do
       before { get :index }
       it { expect(response).to redirect_to(new_user_session_path) }
     end
-  end
 
-  context 'signed in user' do
-    login_user
+    context 'signed in user' do
+      login_user
 
-    describe 'GET #index' do
-      context 'valid url' do
-        it 'queues job' do
-          expect(FaveWorker).to receive(:perform_async)
-            .with(
-              current_user.id.to_s,
-              'http://example.com/hello?source=xyz'
-            ).once
+      it 'queues job' do
+        expect(FaveWorker).to receive(:perform_async)
+          .with(
+            current_user.id.to_s,
+            'http://example.com/hello?source=xyz'
+          ).once
 
-          get :index, url: 'http://example.com/hello?source=xyz'
-        end
+        get :index, url: 'http://example.com/hello?source=xyz'
       end
     end
   end
