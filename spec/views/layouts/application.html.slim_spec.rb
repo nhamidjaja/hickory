fbaf1e88cd01c3d121566487c00e164e4ac01543
Phendy Chandra <phendy@weekendinc.com>
2015-08-05 13:49:19 +0700

    Update google analytic script

--- spec/views/layouts/application.html.slim_spec.rb
+++ spec/views/layouts/application.html.slim_spec.rb
@@ -12,13 +12,13 @@ RSpec.describe 'layouts/application.html.slim', type: :view do
   context 'authorized' do
     let(:user) do
       FactoryGirl.build(:user,
-                         email: 'a@user.com',
-                         authentication_token: 'validtoken',
-                         id: '4f16d362-a336-4b12-a133-4b8e39be7f8e')
+                        email: 'a@user.com',
+                        authentication_token: 'validtoken',
+                        id: '4f16d362-a336-4b12-a133-4b8e39be7f8e')
     end
 
     it 'GA have userId' do
-      allow(view).to receive(:current_user){ user }
+      allow(view).to receive(:current_user) { user }
 
       render template: 'layouts/application.html.slim'
 
