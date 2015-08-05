4f037d55bc413033e53ad0f2d72d50d225ed522c
Phendy Chandra <phendy@weekendinc.com>
2015-08-05 09:46:10 +0700

    Integrate Google Analytic

---
+++ spec/views/layouts/application.html.slim_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'layouts/application.html.slim', type: :view do
+  context 'unauthenticated' do
+    it 'GA not have userId' do
+      render template: 'layouts/application.html.slim'
+
+      expect(rendered).to include('user = auto')
+    end
+  end
+
+  context 'authorized' do
+    let(:user) do
+      FactoryGirl.create(:user,
+                         email: 'a@user.com',
+                         authentication_token: 'validtoken',
+                         id: '4f16d362-a336-4b12-a133-4b8e39be7f8e')
+    end
+
+    it 'GA have userId' do
+      view.stub(:current_user) { user }
+
+      render template: 'layouts/application.html.slim'
+
+      expect(rendered).to include(
+        'user = {"userId":"4f16d362-a336-4b12-a133-4b8e39be7f8e"}')
+    end
+  end
+end
