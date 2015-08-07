2c6ed45f22a3c891687697ecd8722d5c3c1848a4
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-08-07 17:56:32 +0700

    Test that user's full name is included in the welcome email

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
   describe '.welcome' do
-    let(:user) { FactoryGirl.build(:user, email: 'x@yz.com') }
+    let(:user) { FactoryGirl.build(:user, email: 'x@yz.com', full_name: 'John Doe') }
     let(:mail) { UserMailer.welcome(user) }
 
     it { expect(mail.from).to eq(['hello@readflyer.com']) }
     it { expect(mail.to).to eq(['x@yz.com']) }
     it { expect(mail.subject).to eq('Welcome to Flyer') }
+    it { expect(mail.body.encoded).to include('John Doe')}
   end
 end
