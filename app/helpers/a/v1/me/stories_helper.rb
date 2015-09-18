d09b84695141b5352a479d454f40028cc3c52f46
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-09-18 13:31:17 +0700

    Get user's stories through API

---
+++ app/helpers/a/v1/me/stories_helper.rb
@@ -0,0 +1,8 @@
+module A
+  module V1
+    module Me
+      module StoriesHelper
+      end
+    end
+  end
+end
---
+++ spec/controllers/a/v1/me/stories_controller_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe A::V1::Me::StoriesController, type: :controller do
+end
