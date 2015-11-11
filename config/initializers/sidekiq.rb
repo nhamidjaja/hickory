f6d5ee02ab470984da930980ff931f91cf93b4de
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-11-11 20:34:45 +0700

    Customize sidekiq configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,8 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/12' }
+  config.redis = { url: "redis://#{Figaro.env.redis_host!}:6379/0", namespace: 'hickory' }
+  config.failures_max_count = 100000
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/12' }
+  config.redis = { url: "redis://#{Figaro.env.redis_host!}:6379/0", namespace: 'hickory' }
 end
