beea54c88ebc4daf307c31d9e435e97e67b16a95
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-11-12 12:00:13 +0700

    Remove unused config variable for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,5 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{Figaro.env.redis_host!}:6379/0", namespace: 'hickory' }
-  config.failures_max_count = 100000
 end
 
 Sidekiq.configure_client do |config|
