27c46040d57a2cb3199ea53f0c100eeae8840bf5
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-11-20 16:26:10 +0700

    Capistrano integration for whenever crontab

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,9 @@ lock '3.4.0'
 set :application, 'hickory'
 set :repo_url, 'git@gitlab.com:nhamidjaja/hickory.git'
 
+# whenever crontab
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+
 # https://github.com/seuros/capistrano-sidekiq/issues/49
 # ERROR: no tty present and no askpass program specified
 set :sidekiq_monit_default_hooks, false
