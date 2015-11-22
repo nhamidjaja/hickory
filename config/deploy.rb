6a9a3335d28d62f79432acf240d6992374d9cf68
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-11-22 17:46:06 +0700

    Fix capistrano binstub conflict

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,6 @@ set :linked_files, fetch(:linked_files, []).push(
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :linked_dirs, fetch(:linked_dirs, []).push(
-  'bin',
   'log', 
   'tmp/pids', 
   'tmp/cache', 
@@ -48,6 +47,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push(
   'vendor/bundle', 
   'public/system', 
   'public/uploads')
+
+# http://stackoverflow.com/questions/26151443/capistrano-3-deployment-for-rails-4-binstubs-conflict
+set :bundle_binstubs, nil
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
