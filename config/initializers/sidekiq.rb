dfada8bc9dddde4a3923ace74d769d8e2feec694
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2015-12-10 10:37:41 +0700

    Use password authentication for redis server

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,4 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{Figaro.env.redis_host!}:6379/0", namespace: 'hickory' }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{Figaro.env.redis_host!}:6379/0", namespace: 'hickory' }
+  config.redis = { url: "redis://#{Figaro.env.redis_host!}:6379/0", namespace: 'hickory',
+    password: Figaro.env.redis_password }
 end
