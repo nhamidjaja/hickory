506300d8c553b439b9714e8238ffd4d073f0932e
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2016-03-08 19:43:40 +0700

    Count article view in GA

--- app/workers/view_article_worker.rb
+++ app/workers/view_article_worker.rb
@@ -5,5 +5,12 @@ class ViewArticleWorker
     CUserFave.new(c_user_id: Cequel.uuid(faver_id),
                   id: Cequel.uuid(attribution_id))
              .increment_view
+
+    GoogleAnalyticsApi.new.event(
+      'article',
+      viewer_id,
+      faver_id + '/' + attribution_id,
+      1,
+      viewer_id)
   end
 end
