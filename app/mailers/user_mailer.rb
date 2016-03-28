92cc90332b62d8cc2ba0acedcde0a39f3130e8d8
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2016-03-28 13:14:52 +0700

    Email for top curator contest

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,10 @@ class UserMailer < ApplicationMailer
     @user = user
     mail(to: @user.email, subject: 'Welcome to Flyer')
   end
+
+  def tcc_announce(user)
+    @user = user
+    mail(to: @user.email, subject: 'Baca berita bisa dapat hadiah dengan '\
+     'aplikasi Flyer')
+  end
 end
