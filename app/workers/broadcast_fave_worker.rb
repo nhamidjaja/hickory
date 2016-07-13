0d2cafd99408a00c24a7507d034a50309dbc329f
Nicholas Hamidjaja <nhamidjaja@gmail.com>
2016-07-13 16:13:09 +0700

    Worker to broadcast Fave over FCM

---
+++ app/workers/broadcast_fave_worker.rb
@@ -0,0 +1,30 @@
+class BroadcastFaveWorker
+  include Sidekiq::Worker
+
+  def perform(registration_token, username, article_title)
+    fcm = FCM.new(Figaro.env.fcm_server_key!)
+
+    options = { notification: {
+      body: "@#{username} faved '#{article_title}'"
+    } }
+    response = fcm.send([registration_token], options)
+
+    destroy_unregistered_token(response[:not_registered_ids].first)
+    update_canonical_token(registration_token, response[:canonical_ids].first)
+
+    raise response[:response] unless response[:response].eql?('success')
+  end
+
+  private
+
+  def destroy_unregistered_token(unregistered)
+    Gcm.where(registration_token: unregistered).destroy_all if unregistered
+  end
+
+  def update_canonical_token(original, canonical)
+    if canonical
+      gcm = Gcm.find(original)
+      gcm.update_attributes!(registration_token: canonical)
+    end
+  end
+end
---
+++ spec/workers/broadcast_fave_worker_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+require 'sidekiq/testing'
+
+RSpec.describe BroadcastFaveWorker do
+  describe '.perform' do
+    let(:worker) { BroadcastFaveWorker.new }
+    let(:fcm) { instance_double('FCM') }
+
+    before do
+      allow(FCM).to receive(:new)
+        .and_return(fcm)
+    end
+
+    it 'sends notification' do
+      expect(fcm).to receive(:send)
+        .with(['token'],
+              notification: { body: '@username faved \'Some News Headline\'' })
+        .and_return(response: 'success',
+                    canonical_ids: [],
+                    not_registered_ids: [])
+
+      worker.perform(
+        'token',
+        'username',
+        'Some News Headline'
+      )
+    end
+
+    it 'deletes unregistered tokens' do
+      allow(fcm).to receive(:send)
+        .and_return(response: 'success',
+                    canonical_ids: [],
+                    not_registered_ids: ['token'])
+
+      double = instance_double('ActiveRecord::Relation')
+      expect(Gcm).to receive(:where)
+        .with(registration_token: 'token')
+        .and_return(double)
+      expect(double).to receive(:destroy_all)
+
+      worker.perform(
+        'token',
+        'username',
+        'Some News Headline'
+      )
+    end
+
+    it 'updates token' do
+      allow(fcm).to receive(:send)
+        .and_return(response: 'success',
+                    canonical_ids: ['canon'],
+                    not_registered_ids: [])
+
+      gcm = instance_double('Gcm')
+      expect(Gcm).to receive(:find)
+        .with('token')
+        .and_return(gcm)
+
+      expect(gcm).to receive(:update_attributes!)
+        .with(registration_token: 'canon')
+
+      worker.perform(
+        'token',
+        'username',
+        'Some News Headline'
+      )
+    end
+
+    it 'fails on error' do
+      allow(fcm).to receive(:send)
+        .and_return(response: 'Server is temporarily unavailable.',
+                    canonical_ids: [],
+                    not_registered_ids: [])
+
+      expect do
+        worker.perform(
+          'token',
+          'username',
+          'Some News Headline'
+        )
+      end.to raise_error(RuntimeError, 'Server is temporarily unavailable.')
+    end
+  end
+end
